1559fef07eeda38756e4cbc944bb09202fe74bd2
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 16:23:54 +0530

    valid last name

---
+++ lastname.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    internal class lastname
+    {
+        public void Checklastname(string Lastname)
+        {
+            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
+            if (Regex.IsMatch(Lastname, lastnamepattern))
+                Console.WriteLine("Last Name is valid");
+            else
+                Console.WriteLine("Invalid name!!");
+        }
+    }
+}
