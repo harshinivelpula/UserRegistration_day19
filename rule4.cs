a5ecd90052a1a0e8e5b21cec5e3ab9a40cdeac15
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 17:00:16 +0530

    1 special character

---
+++ rule4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    internal class rule4
+    {
+        public void Checkpassword(string Password)
+        {
+            string passwordpattern = "^[A-Za-z0-9]{8,20}$";
+            string passwordpattern1 = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            string passwordpattern2 = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwordpattern3 = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
+            if (Regex.IsMatch(Password, passwordpattern))
+                Console.WriteLine("Password is valid");
+            else
+                Console.WriteLine("Invalid password!!");
+        }
+    }
+}
