2cde7757f513af583d41c5897f96e52215ab5f36
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 16:48:40 +0530

    password rule2

---
+++ rule2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    internal class rule2
+    {
+    }
+}
