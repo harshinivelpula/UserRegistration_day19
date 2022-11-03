5bb3b06dd90b400a6ca7a9ff8a1f3a642fdbdc1d
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 16:33:21 +0530

    valid email

--- email.cs
+++ email.cs
@@ -9,22 +9,7 @@ namespace UserRegistration1
 {
     internal class email
     {
-        public void Checkfirstname(string firstname)
-        {
-            string Firstnamepattern = "^[A-Z][a-z]{3,10}$";
-            if (Regex.IsMatch(firstname, Firstnamepattern))
-                Console.WriteLine("First Name is valid");
-            else
-                Console.WriteLine("Invalid name!!");
-        }
-        public void Checklastname(string Lastname)
-        {
-            string lastnamepattern = "^[A-Z][a-z]{3,10}$";
-            if (Regex.IsMatch(Lastname, lastnamepattern))
-                Console.WriteLine("Last Name is valid");
-            else
-                Console.WriteLine("Invalid name!!");
-        }
+        
         public void Checkemail(string Emailid)
         {
             string emailpattern = "^[a-z][a-zA-Z0-9._-]{3,20}@[a-z]{3,10}.(com)$";
