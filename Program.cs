299bb6961232cc715ce7ddc5ab393cbe3b35a974
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 16:16:44 +0530

    first number

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using UserRegistration1;
+
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Validate first name ");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("First letter should be capital");
+                        Console.WriteLine("length should be Minimum three characters");
+                        Console.WriteLine("Enter First name");
+                        string fname = Console.ReadLine();
+                        FirstNumber Checkpattern = new FirstNumber();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
