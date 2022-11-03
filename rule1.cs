e176ba7d43d90cf3cc68c5bc81a7fb8ba147c0bd
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-03 16:37:21 +0530

    password rule1

---
+++ rule1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    internal class rule1
+    {
+        public void Checkpassward(string Passward)
+        {
+            string passwardpattern = "^[A-Za-z0-9]{8,20}$";
+            if (Regex.IsMatch(Passward, passwardpattern))
+                Console.WriteLine("Passward is valid");
+            else
+                Console.WriteLine("Invalid passward!!");
+        }
+    }
+}
